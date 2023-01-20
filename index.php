054ce6412dc99d0c3b1bcb8f5b79006b8730d499
George Hall <georgehall@george.local>
2023-01-20 11:56:05 +0000

    Start of index.php

---
+++ index.php
@@ -0,0 +1,78 @@
+   <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Meal Mapper</title>
+      <script src="https://kit.fontawesome.com/ea02caf681.js" crossorigin="anonymous"></script>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+      <link rel="stylesheet"
+         href="https://fonts.googleapis.com/css?family=Tangerine">
+         <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+      <style>
+         body {
+          background-color: #ffffff; 
+         }
+         .dropdown:hover .dropdown-menu{
+         display: block;
+         }
+         .dropdown-menu{
+         margin-top: 0;
+         }
+
+      </style>
+      <script>
+         $(document).ready(function(){
+             $(".dropdown").hover(function(){
+                 var dropdownMenu = $(this).children(".dropdown-menu");
+                 if(dropdownMenu.is(":visible")){
+                     dropdownMenu.parent().toggleClass("open");
+                 }
+             });
+         });     
+      </script>
+   </head>
+   <body>
+<!-- centred logo start -->
+      <ul class="nav justify-content-center" style="padding-top: 0px;">
+      <a href ="index.php">  <img src="images/logo.png" alt="logo" style="height:150px;"></a>
+      </ul>
+<!-- centred logo end -->
+<!-- socials top right start -->
+      <div class="d-flex justify-content-center" style="padding-top:20px;">
+      <a href ="https://www.w3schools.com">  <i class="fa-brands fa-facebook fa-2xl " style="padding-right: 5px; color: black;"></i></a>
+      <a href ="https://www.w3schools.com">  <i class="fa-brands fa-twitter fa-2xl" style="padding-right: 5px; color: black;"></i></a>
+      <a href ="https://www.w3schools.com">   <i class="fa-brands fa-instagram fa-2xl" style="padding-right: 15px; color: black;"></i></a>
+      </div>
+<!-- socials top right end -->
+<!-- Navbar start -->
+<div class="d-flex justify-content-center">
+<nav class="navbar navbar-expand-lg justify-content-center" style="padding-top: 20px;">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <div class="d-flex justify-content-center">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="orderfb.php" style="text-align: center">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="dutyfree.php" style="text-align: center">How It Works</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="bookservice.php" style="text-align: center">Log In</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </div>
+    </nav>
+        </div>
+<!-- navbar end -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+   </body>
+   </html>
